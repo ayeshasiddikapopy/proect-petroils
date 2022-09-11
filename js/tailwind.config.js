9827f5af368c356d502d99f2a8fbe1b53fb1e246
ayesha siddika popy <99285521+ayeshasiddikapopy@users.noreply.github.com>
2022-09-11 22:01:51 +0600

    updated config code

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -1,20 +1,20 @@
-tailwind.config ={
+tailwind.config = {
     theme: {
       extend: {
         colors: {
-          clifford: '#da373d',
-          pink : '#ECC5FB',
           black: '#000000',
-          red: '#F40404'
+          red: '#F40404',
+          overlay:'rgba(0, 0, 0, 0.6)',
+          ash:' rgba(240, 240, 240, 1)',
         },
         fontFamily: {
             poppins: ['Poppins', 'sans-serif'],
           },
           backgroundImage: {
-            'hero-pattern': "url('/images/banner.png')",
+            banner: "url('/images/banner.png')", 
           },
           maxWidth: {
-            container: '1170px',
+            container: '1140px',
           }
       }
     }
